faa100930ae535eba18f5bc16bf7df6ebc49e299
ClementJosse <clement.b.josse@gmail.com>
2023-11-13 17:12:34 +0100

    Ajout de l'id de l'appartement, lien de l'annonce, lien de la première image pour toute balise (grid) et prix du csv d'input ;;;; modification du csv d'input en conséquence

--- scraping/seloger/seloger/spiders/linkscraperspider.py
+++ scraping/seloger/seloger/spiders/linkscraperspider.py
@@ -8,26 +8,37 @@ class LinkScraperSpider(scrapy.Spider):
         # Charger les liens depuis le CSV
         with open('3annonces_links.csv', 'r') as file:
             reader = csv.reader(file)
+            # Ignorer la première ligne du CSV (entête)
+            next(reader, None)
             for row in reader:
                 link = row[0]  # La première colonne contient les liens
+                price = row[1]  # La deuxième colonne contient les prix
                 if link:
-                    yield scrapy.Request(url=link, callback=self.parse)
+                    yield scrapy.Request(url=link, meta={'price': price}, callback=self.parse)
 
     def parse(self, response):
-        # Ajouter ici la logique pour extraire les données de la page
-        # Exemple : récupérer le titre de la page
-        title = response.css('title::text').get()
+        # Extraire l'ID de l'appartement du lien
+        apartment_id = response.url.split('-')[-1].split('.')[0]
 
-        # Exemple : récupérer le contenu de la page
-        content = response.css('body::text').get()
+        # Extraire le lien de l'image
+        image_url = response.css('div.grid-3 a.classified-medias__picture picture img::attr(src)').get()
+        # Si le sélecteur initial ne renvoie aucun résultat, chercher dans 'grid-2' puis 'grid-1'
+        if not image_url:
+            image_url = response.css('div.grid-2 a.classified-medias__picture picture img::attr(src)').get()
+            if not image_url:
+                # Changer le sélecteur pour rechercher dans <classified-medias grid-no> <picture data-v-57a34d75> <img::attr(src)>
+                image_url = response.css('section.classified-medias.grid-no img::attr(src)').get()
 
-        # Exemple : récupérer le prix de l'annonce
-        price = response.css('span.price::text').get()
-
-        # Ajouter d'autres logiques de récupération de données selon vos besoins
+        # Récupérer le prix depuis les méta-données
+        price = response.meta.get('price', '')
 
         # Écrire les résultats dans le nouveau CSV
         with open('dataset.csv', 'a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            # Ajouter les colonnes que vous avez extraites de la page
-            writer.writerow([title, content, price, response.url])
+
+            # Si le fichier est vide, écrire l'entête
+            if file.tell() == 0:
+                writer.writerow(['ID', 'Link', 'ImageURL', 'Price'])
+
+            # Ajouter les colonnes extraites de la page
+            writer.writerow([apartment_id, response.url, image_url, price])
